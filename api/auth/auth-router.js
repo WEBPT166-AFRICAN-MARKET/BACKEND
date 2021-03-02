54f2ec462956f076fe549ec5087c4eb39a874afb
Mark Hillin <mark.hillin@gmail.com>
2021-03-02 17:16:53 -0600

    fixed login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -28,23 +28,24 @@ router.post('/register', (req, res) => {
   }
 })
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  try {
-    if (!isValid(req.body)) {
-      next({ apiCode: 400, apiMessage: 'invalid credentials' })
-    } else {
-      const [user] = await Users.findBy({ username: username });
-      if (user && bcrypt.compareSync(password, user.password)) {
+  if(Users.isValid(req.body)) {
+    Users.findBy({username})
+    .then(([ user ]) => {
+      if(user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-        res.status(200).json({ message: `welcome, ${user.username}`, token })
+        res.status(200).json({ message:  `Welcome, ${user.username}`, token: token})
       } else {
-        next({ apiCode: 400, apiMessage: 'invalid credentials' });
+        res.status(401).json({ message: 'Please enter the correct username and password' })
       }
-    } 
-  } catch (err) {
-    next({apiCode: 500, apiMessage: 'invalid credentials'})
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    })
+  } else {
+    res.status(400).json({ message: 'Please provide a username and password' })
   }
 });
 
