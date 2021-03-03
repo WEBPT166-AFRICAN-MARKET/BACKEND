75152935635ca8b01b236cf9e4d0add6279ec5bd
Mark Hillin <mark.hillin@gmail.com>
2021-03-03 09:15:21 -0600

    Create auth-router.test.js

---
+++ api/auth/auth-router.test.js
@@ -0,0 +1,34 @@
+const server = require('../server.js');
+const db = require('../../database/dbConfig');
+const request= require('supertest');
+
+describe('server.js', () => {
+    test('checks for the testing environment', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+})
+
+describe('post /auth/register', () => {
+    let res = {};
+
+    beforeAll(async () => {
+        //await db('users').truncate();
+        res = await request(server).post('/api/auth/register').send({ username: 'mhillin', password: 'password123' })
+        console.log(res)
+    });
+
+    it('uses json', () => {
+        expect(res.type).toBe('application/json')
+    })
+});
+
+describe('post /auth/login', () => {
+    beforeAll(async () => {
+        res = await request(server).post('/api/auth/login').send({  username: 'mhillin', password: 'password123'  });
+        console.log(res.body)
+    });
+
+    it('uses json', () => {
+        expect(res.type).toBe('application/json');
+    })
+});
